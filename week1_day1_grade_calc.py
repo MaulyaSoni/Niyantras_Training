6be2c09fcc21efb2d30fd108363321a7128dd5b8
MaulyaSoni <24maulya@gmail.com>
2026-07-22 17:46:44 +0530

    Day2 decorators and closures completed and Day1 according to changes for improvements

--- week1_day1_grade_calc.py
+++ week1_day1_grade_calc.py
@@ -1,49 +1,53 @@
-# Week 1 , Day 1  
+# WEEK 1 - DAY 1 - PYTHON 
 print("Welcome to the Grade Calculator Application Tool (CLI MODE)")
-
 grade_dict = {}
 student = {}
-id = input("Enter your id :")
+id_no = input("Enter your id :")
 name = input("Enter your name:")
-
-student[id]=name
+student[id_no]=name
 
 
 n = int(input("\nEnter the number of subjects: "))
-for _ in range(n):
-    subj = input("Enter subject: ")
-    marks = int(input("Enter marks: ")) 
-    grade_dict[subj] = marks
 
-# Average 
-total = 0
+if(n == 0):
+    print("!!! Invalid input !!! , Please enter at least one Subject to calculate the average")
 
-for subj, marks in grade_dict.items(): 
-    total += marks  
-
-avg = total / n
-print(avg , total)
-
-# Grade Calculation Part
-if avg >= 90 and avg <= 100:
-   grade = 'Grade AA'
-elif avg >= 80 or avg < 90:
-    grade = 'Grade  AB'
-elif avg >= 70 and avg < 80:
-    grade = 'Grade BB'
-elif avg >= 60 and avg < 70:
-    grade = 'Grade BC'
-elif avg >=50 and avg < 60:
-    grade = 'Grade CC'
 else:
-    grade = 'Grade D'
+
+    for _ in range(n):
+        subj = input("Enter subject: ")
+        marks = int(input("Enter marks between (0-100) : ")) 
+        grade_dict[subj] = marks
+        
+    # Average 
+    total = 0
+
+    for subj, marks in grade_dict.items(): 
+        total += marks  
+
+    avg = total / n
+    # print(f"Your total score is {total} & average score is {avg}")
+
+    # Grade Calculation Part
+    if avg >= 90 and avg <= 100:
+        grade = 'AA'
+    elif avg >= 80 and avg < 90:
+        grade = 'AB'
+    elif avg >= 70 and avg < 80:
+        grade = 'BB'
+    elif avg >= 60 and avg < 70:
+        grade = 'BC'
+    elif avg >=50 and avg < 60:
+        grade = 'CC'
+    else:
+        grade = 'D'
 
 
-# Concatination of the Dictionaries
+    # Concatination of the Dictionaries
 
-student = student | grade_dict
-student[avg] = grade
+    student |= grade_dict
 
-# print(student)
+    # print(student)
 
-print(f"\n Grand Total of the student {id} {name} is {total} and it's average marks is {avg}\n")
+    print(f"\nDetails of student : {id_no} {name} \nTotal Score : ({total}/{n*100}) \nAverage marks : {round(avg,3)} \nGrade : {grade}\n")
+    
