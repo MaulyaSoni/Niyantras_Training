d0d31e4539464ec8a700960cd842f516dcd06e57
MaulyaSoni <24maulya@gmail.com>
2026-07-27 17:56:34 +0530

    Day 4 Task  completed

--- week1_day3.py
+++ week1_day3.py
@@ -1,23 +1,3 @@
-# # timer using Closures
-# from time import time
-# def func_time(func):
-#     def wrap_time(*args):
-#         t1 = time()
-#         result = func(*args)
-#         t2 = time()
-#         print(f"Time to execute {func.__name__!r} is {(t2-t1):.2f}s\n")
-#         return result
-#     return wrap_time
-
-# # Closures
-# def data_collect(func):
-#     def wrapper(*args) :
-#         print("Processing and Collecting your Data...")
-#         result = func(*args)
-#         print("Data Capturing Part is done")
-#         return result
-#     return wrapper
-
 def operations(func):
     def wrapper(*args):
         print("\n...Algorithm is running...")
@@ -26,13 +6,6 @@ def operations(func):
         return result
     return wrapper
 
-# def end_part(func):
-#     def wrapper(*args,**kwargs):
-#         print("..........Final Result..........")
-#         result = func(*args,**kwargs)
-#         print(".........THANK YOU !!!..........")
-#         return result
-#     return wrapper
 
 print("\n...Welcome to the Grade Calculator Application Tool (CLI MODE)...")
 
@@ -66,6 +39,7 @@ class Student:
         return f"\nDetails of student : \n Student_ID : {self.s_id}\n Student Name : {self.name}  \n  Average marks : {self.avg} \n Grade : {self.grade}\n"
 
 flag = True
+
 class Details:
     def __init__(self):
         pass
@@ -83,66 +57,67 @@ class Details:
             grade_dict[subj] = marks
         return grade_dict
 
+class Calculations:
+    def __init__(self):
+        pass
+
+    def avg(self,n , grades):
+        self.n = n
+        self.grades = grades
+        total = sum(grades.values())
+        average = total/n
+        average= round(average,3)
+        # print(f"After avg {average}")
+        return average
+
+calc = Calculations()
+average_func = calc.avg(n,grades = detail_dict)
+
+class Rank:
+    def __init__(self):
+        pass
+    # Function for Calculating GRADE
+    def grade_func(self,avg):
+        self.avg = avg
+        grade =''
+
+        if avg >= 90 and avg <= 100:
+            grade = 'AA'
+        elif avg >= 80 and avg < 90:
+            grade = 'AB'
+        elif avg >= 70 and avg < 80:
+            grade = 'BB'
+        elif avg >= 60 and avg < 70:
+            grade = 'BC'
+        elif avg >=50 and avg < 60:
+            grade = 'CC'
+        else:
+            grade = 'D'
+        return grade
+
 S_id = input("\nEnter the ID of the student : ")
 std_name = input("Enter the Name of the Student : ")
+
 if not std_name.isnumeric():
 
     n = int(input("Enter the number of Subjects : "))
     if n>0:
         subj_obj= Subject(n)
 
-        print(subj_obj)
-        # subj_obj.subj = 4
         if flag == True:
+            
+            #Details class object call
             det = Details()
             detail_dict = det.data(n)
-
-            class Calculations:
-                def __init__(self):
-                    pass
-                
-                def avg(self,n , grades):
-                    self.n = n
-                    self.grades = grades
-                    total = sum(grades.values())
-                    average = total/n
-                    average= round(average,3)
-                    # print(f"After avg {average}")
-                    return average
-
-            calc = Calculations()
-            average_func = calc.avg(n,grades = detail_dict)
-
-            class Rank:
-                def __init__(self):
-                    pass
-                # Function for Calculating GRADE
-                def grade_func(self,avg):
-                    self.avg = avg
-                    grade =''
-
-                    if avg >= 90 and avg <= 100:
-                        grade = 'AA'
-                    elif avg >= 80 and avg < 90:
-                        grade = 'AB'
-                    elif avg >= 70 and avg < 80:
-                        grade = 'BB'
-                    elif avg >= 60 and avg < 70:
-                        grade = 'BC'
-                    elif avg >=50 and avg < 60:
-                        grade = 'CC'
-                    else:
-                        grade = 'D'
-                    return grade
-
-                # Rounding the average to 3 decimal points
+            # Rank class object call
             rnk = Rank()
             grade = rnk.grade_func(average_func)
 
             # STUDENT Class object calling 
             stu = Student(S_id , std_name ,average_func , grade)
             print(stu)
-        else :
+
+        else:
             print("Marks can be entered only between 0-100")
     else:
         print("Subjects can't be less than Zero or Negative ")
---
+++ week1_day4.py
@@ -0,0 +1,125 @@
+from abc import ABC  , abstractmethod
+
+class StudentDetails:
+    def __init__(self):
+        print("\n...Welcome to the Grade Calculator Application Tool (CLI MODE)...")
+    def card(self):
+        s_id = input("\nEnter the ID of the student : ")
+        std_name = input("Enter the Name of the Student : ")
+        return s_id , std_name
+
+# Abstract Class 
+class Student(ABC):
+    def __init__(self, s_id , name ):
+        self.s_id = s_id
+        self.name = name
+
+    @abstractmethod 
+    def data(self):
+        pass    
+
+# Composite Class 1 
+class MarksDetails(Student):
+    def __init__(self):
+        print("MarksDetails Class object created ")
+
+    def data(self,n):
+        self.n = n 
+        global flag
+        grade_dict={}
+        for _ in range(n):
+            subj = input("\nEnter name of the Subject : ")
+            marks = int(input("Enter marks between (0-100): "))
+
+            if marks > 100 or marks < 0:
+               flag = False
+            grade_dict[subj] = marks
+        
+        return grade_dict
+
+# Composite Class 2
+class Calculations:
+    def __init__(self):
+        #object of MarksDetails class
+        self.det = MarksDetails()
+        print("Details class object created")
+
+    def avg(self,n):
+        #Composition instance calling
+        grades = self.det.data(n)
+        self.n = n
+     
+        total = sum(grades.values())
+        average = total/n
+        average= round(average,3)
+        
+        return total , average
+
+# Composite class 3
+class Rank:
+    def __init__(self):
+        print("Rank class object created")
+
+    def grade_func(self):
+        grade =''
+        if avg >= 90 and avg <= 100:
+            grade = 'AA'
+        elif avg >= 80 and avg < 90:
+            grade = 'AB'
+        elif avg >= 70 and avg < 80:
+            grade = 'BB'
+        elif avg >= 60 and avg < 70:
+            grade = 'BC'
+        elif avg >=50 and avg < 60:
+            grade = 'CC'
+        else:
+            grade = 'D'
+        return grade
+
+class Result:
+    def __init__(self,s_id,name,avg,grade,t_marks,n):
+        print("\n.....Printing your result......")
+        self.s_id=s_id
+        self.name = name
+        self.avg = avg
+        self.grade  = grade
+        self.t_marks = t_marks
+        self.n = n           
+    def __str__(self):
+        return f"\n Details of student : \n Student ID : {self.s_id}\n Student Name : {self.name}  \n Total Marks :{self.t_marks}/{self.n*100} \n Average marks : {self.avg} \n Grade : {self.grade}\n"
+
+# main part 
+flag = True 
+sd = StudentDetails()
+s_id , std_name = sd.card()
+
+# name condition 
+if not std_name.isnumeric():
+    # number of subject condition 
+    n = int(input("Enter the Number of Subjects : "))
+    if n>0:
+        cal_obj= Calculations()
+        t_marks , avg = cal_obj.avg(n)
+
+        # marks negavtive condition
+        if flag == True:
+            rnk= Rank()
+            gr = rnk.grade_func()
+
+            res = Result(s_id,std_name,avg,gr,t_marks,n)
+            print(res)
+
+        else:
+            print("Marks can be entered only between 0-100")
+    else:
+        print("Subjects can't be less than Zero or can't be Negative ")
+else:
+    print("Name can't be contain any numbers")
+
+
+# Note :
+# Why choose the Composition over the Inheritance ?
+# By choosing the composite class you can do complex task with less brainstoming by having a class in another class by defining the instances in that class.
+# By this the maintainence of the code increases , and class hierarchical complexity decreases.
+# It gives you flexibility to resolve major code changes by changing less lines of code , along with reusability we are getting , which helps us to use at any part of code without making the inheritance and complex codes.
+# When an object or the thing need other's behaviour for making the simple way to the output with less complexity and maintanence , they use the Composite Classes.   
